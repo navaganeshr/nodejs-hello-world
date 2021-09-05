2f4b64465ea78f3d0a01608a493c9d6efceffb72
Navaganesh R <navaganeshr69@gmail.com>
2021-09-06 02:29:05 +0530

    chart addition and few refactoring

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 8080
+EXPOSE 3000
 CMD [ "npm" , "start" ]
--- src/app.js
+++ src/app.js
@@ -6,9 +6,6 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-console.log('Visit http://localhost:5000/')
-console.log('Press Ctrl+C to shut down...')
-
-app.listen(process.env.PORT || 5000);
-
+console.log('Visit http://localhost:3000/')
+app.listen(process.env.PORT || 3000);
 module.exports = app;
